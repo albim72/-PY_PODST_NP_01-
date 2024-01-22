3d8e0952e0ed21182086f688f8392b39ba0f8854
albim72 <marcin.albiniak72@gmail.com>
2024-01-22 12:04:57 +0100

    Create input.py

---
+++ DZIEN_1/input.py
@@ -0,0 +1,8 @@
+print("Witaj użtkowniku")
+print("Podaj swoje imię... ")
+imie = input()
+print("Miło Cię widzieć " + imie)
+print("Imię ma znaków: " + str(len(imie)))
+
+wiek = int(input("Podaj swoj wiek [lata]: "))
+print("W przysszłym roku będiesz mieć lat: " + str(wiek+1))
