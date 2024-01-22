f7c88f03358d0bc59d8cf67e94612f407bd247e1
albim72 <marcin.albiniak72@gmail.com>
2024-01-22 11:17:32 +0100

    Create podstawy.py

---
+++ DZIEN_1/podstawy.py
@@ -0,0 +1,39 @@
+print("pierwsza linia programu Python")
+
+tekst = "to jest zmienna tekstowa(string)"
+print(tekst)
+print(type(tekst))
+
+info:str = "ważna informacja 123"
+print(info)
+print(type(info))
+
+info = 123
+print(info)
+print(type(info))
+
+a = 560
+print(a)
+print(type(a))
+print(id(a))
+
+a = 7.7
+print(a)
+print(type(a))
+print(id(a))
+
+
+
+#to jest komentarz jednolinowy
+"""
+to jest komentarz wieloliniowy,
+jeśli występuje pierwszy raz w pliku Pythona to jest również
+komentarzem dokumentacyjnym
+"""
+'''
+CTRL+ / -> komentowanie/odkomentowanie bloku kodu
+CTRL+D -> duplikacja linii/bloku kodu
+'''
+
+
+
