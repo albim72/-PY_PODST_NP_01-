2488ccc10dc9cb220d6284f416cd34959cf7fafd
albim72 <marcin.albiniak72@gmail.com>
2024-01-22 13:37:52 +0100

    Update listy.py

--- DZIEN_1/listy.py
+++ DZIEN_1/listy.py
@@ -42,3 +42,58 @@ print(sklepzoo[0])
 print(sklepzoo[0][1])
 print(sklepzoo[0][0])
 print(sklepzoo[0][0][0], "-",sklepzoo[1][0][0]," zł")
+
+#konkatenacja tablic
+miasta = ["Kielce","Radom","Lublin","Rzeszów"]
+stolica = ["Warszawa","Rzym","Londyn"]
+
+print(miasta)
+print(stolica)
+
+miasta = miasta + stolica
+
+print(miasta)
+
+m = miasta + ["Zamość","Koszalin"]
+
+print(m)
+
+miasta.remove("Radom")
+print(miasta)
+print(m)
+
+#operacje na listach
+litery = ['a','b','c','d','e','f','g','h','x','y','z']
+print("przed zmianą: ", litery)
+litery[2:7] = [98,53,23,56,78,34,101]
+print("po zmianie: ", litery)
+
+litery_x = litery
+#nowe listy
+litery_p = list(litery)
+litery_q = litery[:]
+litery_z = litery_p.copy()
+
+print("litery_x przed zmianą: ", litery_x)
+print("litery przed zmianą: ", litery)
+print("litery_p przed zmianą: ", litery_p)
+print("litery_q przed zmianą: ", litery_q)
+print("litery_z przed zmianą: ", litery_z)
+
+litery[:] = [1001,1002,1006,1010]
+print("___________________________________")
+print("litery_x po zmianie: ", litery_x)
+print("litery po zmianie: ", litery)
+print("litery_p po zmianie: ", litery_p)
+print("litery_q po zmianie: ", litery_q)
+print("litery_z po zmianie: ", litery_z)
+
+print("__________________identyfikatory dla litery i litery_x _________________")
+print(id(litery))
+print(id(litery_x))
+print(id(litery_p))
+print(id(litery_q))
+print(id(litery_z))
+
+
+
