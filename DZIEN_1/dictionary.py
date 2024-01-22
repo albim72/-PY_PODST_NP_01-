0ca8a54b9cbdf0fa1463e3d3b8ed33b8974c473a
albim72 <marcin.albiniak72@gmail.com>
2024-01-22 15:59:24 +0100

    Create dictionary.py

---
+++ DZIEN_1/dictionary.py
@@ -0,0 +1,14 @@
+#słownik -> doctionary => (klucz,wartość)
+
+pojazd = {
+    "marka":"Ford",
+    "model":"Mustang",
+    "rok":1965,
+    "kolor":"czerwony",
+    455:6745634
+}
+
+print(pojazd)
+print(type(pojazd))
+
+print(pojazd["marka"])
