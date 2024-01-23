c12fdc6cc404eb98847c96969b8e01bc594ac7ea
albim72 <marcin.albiniak72@gmail.com>
2024-01-23 09:44:45 +0100

    Create przykladowe_funkcje.py

---
+++ DZIEN_2/przykladowe_funkcje.py
@@ -0,0 +1,14 @@
+#przykład 1
+def witaj():
+    print("witaj użytkowniku")
+    print("witaj w naszej aplikacji...")
+    print("zaloguj się!")
+
+
+witaj()
+p = witaj()
+# p
+print(type(p))
+
+print("___________________________________________________")
+#uruchom 20 razy (iteracyjnie) funkcję witaj()
