9f04c2d0975ad761fd3cab11154a0a2326966d50
albim72 <marcin.albiniak72@gmail.com>
2024-01-23 11:06:00 +0100

    Update funkcje_anonimowe.py

--- DZIEN_2/funkcje_anonimowe.py
+++ DZIEN_2/funkcje_anonimowe.py
@@ -12,3 +12,15 @@ wynik = potega(6)(9)
 print(wynik)
 
 liczby = [8,3,6,12,-4,0,34,113,-45,98,13,2,-5,-66,70,-49,221,3,5]
+
+parzste = list(filter(lambda x:x%2==0,liczby))
+print(parzste)
+
+def ujemne(x):
+    return x<0
+
+ujem = list(filter(ujemne,liczby))
+print(ujem)
+
+cube = list(map(lambda x:x**3,liczby))
+print(cube)
