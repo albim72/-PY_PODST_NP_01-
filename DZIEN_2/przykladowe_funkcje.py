86371124ea22ba69fe9e89e6c70a06431a9823bb
albim72 <marcin.albiniak72@gmail.com>
2024-01-23 10:08:32 +0100

    Update przykladowe_funkcje.py

--- DZIEN_2/przykladowe_funkcje.py
+++ DZIEN_2/przykladowe_funkcje.py
@@ -25,3 +25,17 @@ print(klient(43242343,"Niemcy"))
 print(klient(54654655,"Włochy"))
 print(klient(798677567))
 print(klient('8769455895'))
+
+#przykład 3
+
+g = 10
+def obliczenie(a,b,c):
+    f = (a+b)*c-a*b
+    f=2*f + g
+    return f
+
+print(obliczenie(4,7,4))
+print(obliczenie(5.74,2,0))
+print(obliczenie(-0.0005434,True,700))
+# print(obliczenie('fsdf',True,700))
+
