508bed75b6ae44aaf7612372cbb75e4b21e22d3e
albim72 <marcin.albiniak72@gmail.com>
2024-01-23 14:05:25 +0100

    Update klasa123.py

--- DZIEN_2/klasa123.py
+++ DZIEN_2/klasa123.py
@@ -17,19 +17,39 @@ class DrugaKlasa(KlasaPierwsza):
 
     def policzsume(self):
         print(f"suma a+b+c={self.a + self.b + self.c}")
-        
 
-class TrzeciaKlasa(DrugaKlasa):
-    def __init__(self, a, b, c, d):
-        super().__init__(a, b, c)
+class KlasaSpecjalna:
+    def __init__(self,g):
+        self.g=g
+
+class TrzeciaKlasa(DrugaKlasa,KlasaSpecjalna):
+    def __init__(self, a, b, c, d, g):
+        DrugaKlasa.__init__(self,a, b, c)
+        KlasaSpecjalna.__init__(self,g)
         self.d=d
-        
-    def print_abcd(self):
-        print(f"a={self.a}, b={self.b}, c={self.c}, d={self.d}")
+
+    def print_abcdg(self):
+        print(f"a={self.a}, b={self.b}, c={self.c}, d={self.d}, g={self.g}")
 
     def policzsume(self):
-        print(f"suma a+b+c+d={self.a + self.b + self.c+self.d}") 
-        
-    
-        
-    
+        print(f"suma a+b+c+d+g={self.a + self.b + self.c+self.d+self.g}")
+
+
+print("________ KlasaPierwsza _________")
+
+pk = KlasaPierwsza(34,101)
+pk.print_ab()
+
+print("________ KlasaDruga _________")
+dk = DrugaKlasa(6,17,88)
+dk.print_ab()
+dk.print_abc()
+dk.policzsume()
+
+print("________ KlasaTrzecia _________")
+tk = TrzeciaKlasa(45,23,8,79,11)
+tk.print_ab()
+tk.print_abc()
+tk.print_abcdg()
+tk.policzsume()
+
