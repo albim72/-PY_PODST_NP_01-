4ab4449961171e7e98ca82f0304c33a57b4b9044
albim72 <marcin.albiniak72@gmail.com>
2024-01-23 12:09:11 +0100

    Update books.py

--- DZIEN_2/books.py
+++ DZIEN_2/books.py
@@ -4,6 +4,8 @@ class Book:
     # def __new__(cls, *args, **kwargs):
     #     return object.__new__(Book)
 
+    wydawnictwo:str
+
     def __init__(self,id,tytul,autor,cena=30):
         self.idbook = id
         self.tytul = tytul
@@ -13,7 +15,14 @@ class Book:
         self.create_book()
 
     def __repr__(self):
-        return "książka " + self.tytul + ", autor: " + self.autor
+        return "książka " + self.tytul + ", autor: " + self.autor + ", oprawa:" + self.oprawa
+
+    def get_oprawa(self):
+        return self.oprawa
+
+
+    def set_oprawa(self,nowaoprawa):
+        self.oprawa = nowaoprawa
 
 
     def create_book(self):
@@ -22,11 +31,24 @@ class Book:
     def rabat(self,procent):
         return self.cena*(procent/100)
 
+    def set_wydawnictwo(self,nowew):
+        self.wydawnictwo = nowew
+
+    def get_wydawnictwo(self):
+        return self.wydawnictwo
+
 
 bk = Book(34,"Wiedźmin","Andrzej Sapkowski",43)
 print(bk)
 print("Rabat: " + str(bk.rabat(22)) + " zł")
 print("Kwota do zapłaty: " +str(bk.cena - bk.rabat(22)) + " zł")
+bk.set_oprawa("twarda")
+
+print('zmiana oprawy na: ' + bk.get_oprawa())
+bk.set_wydawnictwo("ABC")
+print("wydawnicto " + bk.get_wydawnictwo())
+
+
 
 
 
