2c703b25bb8f282393e8e34e4dba47d88207fb23
albim72 <marcin.albiniak72@gmail.com>
2024-01-24 13:07:43 +0100

    Create pep8.py

---
+++ DZIEN_3/pep8.py
@@ -0,0 +1,22 @@
+def testowa(a,b):
+    global __g
+    info="ab"
+    _t = "obliczenie nr 1"
+    __g = 56
+    return (a+b)*__g
+
+print(testowa(4,6))
+print(__g)
+
+class Test:
+    def __init__(blabla,x):
+        blabla.x=x
+    __h = 10
+    _hx = 9
+    def liczmy(blabla):
+        return blabla.__h + blabla.x
+
+t = Test(56)
+print(t.liczmy())
+print(t._hx)
+# print(Test.__h)
