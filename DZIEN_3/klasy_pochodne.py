c80c11758a9dc8cdea02004f111a6af907506176
albim72 <marcin.albiniak72@gmail.com>
2024-01-24 10:18:46 +0100

    Update klasy_pochodne.py

--- DZIEN_3/klasy_pochodne.py
+++ DZIEN_3/klasy_pochodne.py
@@ -10,17 +10,29 @@ class Child(Base):
     def __init__(self, imie, wiek):
         super().__init__(imie)
         self.wiek=wiek
-        
+
     def get_wiek(self):
         return self.wiek
-    
-    
+
+
 class GrandChild(Child):
 
     def __init__(self, imie, wiek, miasto):
         super().__init__(imie, wiek)
         self.miasto = miasto
-        
+
+    def __repr__(self):
+        return (f"Wnuczek(GrandChild):\nimię -> {self.get_imie()}\nwiek -> {self.get_wiek()}\n"
+                f"miasto -> {self.get_miasto()}")
+
     def get_miasto(self):
         return self.miasto
-    
+
+
+ob = GrandChild("Bonifacy",19,"Kraków")
+print(ob)
+print(f'dane osoby -> imię: {ob.get_imie()}, wiek: {ob.get_wiek()}, miasto: {ob.get_miasto()}')
+
+print("_____________________________________________________")
+ob2 = GrandChild("Olga",33,"Lublin")
+print(ob2)
