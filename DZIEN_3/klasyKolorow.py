5138812b65d4dbf4d9caf92948488e8c12a6f785
albim72 <marcin.albiniak72@gmail.com>
2024-01-24 11:03:30 +0100

    Create klasyKolorow.py

---
+++ DZIEN_3/klasyKolorow.py
@@ -0,0 +1,6 @@
+#Utwórz klasę dziedzącą jawnie klasę object o nazwie PaletaKolorow(idpalety,liczba_kolorow,nazwa_palety)
+#Utwórz konstruktor klasy uwzględniając dane powyżej -: def __init__()
+#Utwórz metodę get_liczba_kolorow() - zwracającą liczbę kolorów palety
+#Utwórz metodę print_paleta - która wypisze dane palety - id,nazwa
+
+#Utwórz obiekt plt1 i użyj dostępnych metod na obiekcie
