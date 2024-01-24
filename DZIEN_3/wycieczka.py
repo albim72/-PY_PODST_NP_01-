57583f3961a465d11c389ec3447c0a9defc70a9b
albim72 <marcin.albiniak72@gmail.com>
2024-01-24 09:59:24 +0100

    Update wycieczka.py

--- DZIEN_3/wycieczka.py
+++ DZIEN_3/wycieczka.py
@@ -7,3 +7,25 @@ def kwota(t,nw,w,u,i,rab=0):
 print("kwota do zapłaty:",kwota(100,100,50,50,50),"zł")
 print("kwota do zapłaty:",kwota(100,100,50,50,50,25),"zł")
 print("kwota do zapłaty:",kwota(100,100,50,50,50,75),"zł")
+
+#Tworzenie listy z dostępnymi opcjami rabatu (wartości dyskretne)
+rabs = [0,8,10,12,15,20,22,25,35,50]
+
+wynik = []
+for r in rabs:
+    kw = kwota (1700, 1230, 450, 200, 180, r)
+
+    if r == 0:
+        print ("cena bazowa do zapłaty wynosi:",kw,"zł")
+
+    else:
+        print ("dla rabatu:", r, "%, kwota do zapłaty wynosi:", kw,"zł")
+
+    wynik.append(kw)
+
+
+print (wynik)
+
+result = [kwota(1700,1230,450,200,180,r) for r in rabs]
+print(result)
+
