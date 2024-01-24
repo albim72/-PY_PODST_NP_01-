007193f44d5fb8686102d7c7bd76b50a7429bd22
albim72 <marcin.albiniak72@gmail.com>
2024-01-24 09:37:15 +0100

    Update wycieczka.py

--- DZIEN_3/wycieczka.py
+++ DZIEN_3/wycieczka.py
@@ -1,6 +1,9 @@
 def kwota(t,nw,w,u,i,rab=0):
-    return (nw+t)*(1-rab/100)+w+u+i
+    if rab>=0 and rab <= 70:
+        return (nw+t)*(1-rab/100)+w+u+i
+    else:
+        return "podaj rabat z przedziału 1% - 70%"
 
-print("kwota do zapłaty",kwota(100,100,50,50,50),"zł")
-print("kwota do zapłaty",kwota(100,100,50,50,50,25),"zł")
-print("kwota do zapłaty",kwota(100,100,50,50,50,75),"zł")
+print("kwota do zapłaty:",kwota(100,100,50,50,50),"zł")
+print("kwota do zapłaty:",kwota(100,100,50,50,50,25),"zł")
+print("kwota do zapłaty:",kwota(100,100,50,50,50,75),"zł")
