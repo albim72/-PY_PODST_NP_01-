e398e88b590cc40956e6a8fe900d21eeccbc9757
albim72 <marcin.albiniak72@gmail.com>
2024-01-24 11:44:48 +0100

    Update klasyKolorow.py

--- DZIEN_3/klasyKolorow.py
+++ DZIEN_3/klasyKolorow.py
@@ -28,3 +28,26 @@ print(plt1.print_paleta())
 #o pola: id_koloru, nazwa_koloru
 #dodaj metodę print_kolor i wypisz za jej pomocą dane: id_koloru, nazwa_koloru
 #zbuduj obiekty k1 i k2 opisujące dwa równe kolory - użyj wszstkich dostępnych metod dla tych instancji
+
+class Kolory(PaletaKolorow):
+    def __init__(self, idpalety, liczba_kolorow, nazwa_palety, id_koloru, nazwa_koloru):
+        super().__init__(idpalety, liczba_kolorow, nazwa_palety)
+        self.id_koloru = id_koloru
+        self.nazwa_koloru = nazwa_koloru
+
+    def print_kolor(self):
+        return f'id koloru: {self.id_koloru}, nazwa koloru: {self.nazwa_koloru}'
+
+k1 = Kolory(12,90,"S10",34,"czerwony")
+print("___ kolor k1 ____")
+print(f'liczba kolorów w palecie: {k1.get_liczba_kolorow()}')
+print(k1.print_paleta())
+print(k1.print_kolor())
+
+
+k2 = Kolory(555,255,"specjalna",80,"biały")
+print("___ kolor k2 ____")
+print(f'liczba kolorów w palecie: {k2.get_liczba_kolorow()}')
+print(k2.print_paleta())
+print(k2.print_kolor())
+
