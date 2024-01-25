329f30cd06c8b1c2c6759297ff09adfa839aa672
albim72 <marcin.albiniak72@gmail.com>
2024-01-25 11:29:37 +0100

    Create pliki_csv.py

---
+++ DZIEN_4/pliki_csv.py
@@ -0,0 +1,6 @@
+pracownik;stanowisko;urodziny;nagroda
+Jan Nowak;dyrektor;luty;16700
+Anna Kot;dyrektor finansowy;sierpień;14300
+Jacek Kowal;programista;styczeń;13200
+Olgierd Bryń;kierowca;maj;9000
+Beata Kruk;sekretrka;czerwiec;9000
