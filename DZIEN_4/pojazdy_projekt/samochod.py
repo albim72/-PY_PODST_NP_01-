c4aa2a252e6055d9e6b6aabbb9a599ee7a14c5f8
albim72 <marcin.albiniak72@gmail.com>
2024-01-25 10:17:41 +0100

    Update samochod.py

--- DZIEN_4/pojazdy_projekt/samochod.py
+++ DZIEN_4/pojazdy_projekt/samochod.py
@@ -5,8 +5,18 @@ class Samochod(Pojazd):
         super().__init__(marka, model, rodzaj_silnika, rocznik)
         self.poj_bagaznika = poj_bagaznika
 
+    def __repr__(self):
+        return f"reprezentacja tekstowa obiektu: {self.marka} {self.model}"
+
+    def __str__(self):
+        return (f"Samochód: {self.marka} {self.model}\n"
+                f"rodzaj silnika: {self.rodzaj_silnika}\n"
+                f"rocznik: {self.rocznik}\n"
+                f"pojemność bagażnika: {self.poj_bagaznika} l")
+
     def spalanie(self, odl, jedn):
-        return jedn*100*1,1/odl
+        return jedn*100*1.1/odl
 
     def koszt_przejazdu(self, odl, jedn, cena_j):
         return self.spalanie(odl,jedn)*(odl/100)*cena_j
+
