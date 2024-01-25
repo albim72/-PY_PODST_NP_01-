8fcc1a3ec12cfad951a076fc5d73959bfbc568d1
albim72 <marcin.albiniak72@gmail.com>
2024-01-25 11:11:22 +0100

    Update pliki_projekt.py

--- DZIEN_4/pliki_projekt.py
+++ DZIEN_4/pliki_projekt.py
@@ -1,3 +1,5 @@
+import os
+
 f = open("danex.txt","r",encoding="utf-8")
 
 print(f.readline())
@@ -19,3 +21,32 @@ for i in f:
     print(i)
 
 f.close()
+
+print("__________ zapis do pliku - w __________")
+
+f = open("message.txt","w",encoding="utf-8")
+f.write("to jest pierwsza linia\n")
+f.close()
+
+print("__________ zapis do pliku - a __________")
+f = open("info.txt","a",encoding="utf-8")
+f.write("to jest n-ta linia\n")
+f.close()
+
+# print("__________ zapis do pliku - b __________")
+# f = open("bane.txt","bw")
+# f.write()
+# f.close()
+
+print("wyświetlenie informacji z pliku info.txt")
+
+f = open("info.txt","r",encoding="utf-8")
+print(f.read())
+f.close()
+
+if os.path.exists("info.txt"):
+    os.remove("info.txt")
+    print("plik został usunięty")
+else:
+    print("plik nie istnieje!")
+
