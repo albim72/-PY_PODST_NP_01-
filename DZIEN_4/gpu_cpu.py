9b33c10fbb2af78679e5feb6db123acc09838073
albim72 <marcin.albiniak72@gmail.com>
2024-01-25 13:48:26 +0100

    Create gpu_cpu.py

---
+++ DZIEN_4/gpu_cpu.py
@@ -0,0 +1,12 @@
+from numba import jit,cuda
+import numpy as np
+from timeit import default_timer as timer
+
+def cpufunction(a):
+    for i in range(250_000_000):
+        a[i]+=1
+        
+@jit(target_backend='cuda')
+def gpufunction(a):
+    for i in range(250_000_000):
+        a[i]+=1
